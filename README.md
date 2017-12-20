c12f45228c359d721c15d81e3499101e1e5dd57a
Roshan Raj Mishra <mishra.roshanraj@gmail.com>
2017-12-20 20:44:33 +0530

    Assignment

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
 # getting-cleaning-data
 The Course Project for the Getting and Cleaning Data Course at Coursera
 
-This is the course project for the Getting and Cleaning Data Coursera course.
 The R script, `run_analysis.R`, does the following:
 
-1. Download the dataset if it does not already exist in the working directory
-2. Load the activity and feature info
-3. Loads both the training and test datasets, keeping only those columns which
-   reflect a mean or standard deviation
-4. Loads the activity and subject data for each dataset, and merges those
+Step 1. Load the activity and feature info
+Step 2. Loads both the training and test datasets, keeping only those columns which
+   reflect a mean or standard deviation.
+Step 3. Activity and subject data is loaded from each dataset, and merged the selected
    columns with the dataset
-5. Merges the two datasets
-6. Converts the `activity` and `subject` columns into factors
-7. Creates a tidy dataset that consists of the average (mean) value of each
+Step 4. Merges the two datasets
+Step 5. Converts the coloumns `activity` and `subject` to factors
+Step 6. Creates a cleaned dataset that consists of the average (mean) value of each
    variable for each subject and activity pair.
-
-The end result is shown in the file `tidy.txt`.
+Step 7. The cleaned dataset is exported in the file `cleaneddataset.txt`.
---
+++ run_analysis.R
@@ -0,0 +1,46 @@
+library(reshape2)
+
+#loading labels and feat from data
+
+actLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+actLabels[,2] <- as.character(actLabels[,2])
+feat <- read.table("UCI HAR Dataset/features.txt")
+feat[,2] <- as.character(feat[,2])
+
+# Extracts only the measurements on the mean and standard deviation for each measurement
+
+featReq <- grep(".*mean.*|.*std.*", feat[,2])
+featReq.names <- feat[featReq,2]
+featReq.names = gsub('-mean', 'Mean', featReq.names)
+featReq.names = gsub('-std', 'Std', featReq.names)
+featReq.names <- gsub('[-()]', '', featReq.names)
+
+
+# Load the required datasets
+
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[featReq]
+trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
+trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
+
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[featReq]
+testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
+testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(testSubjects, testActivities, test)
+
+# merge datasets and add labels
+
+allData <- rbind(train, test)
+colnames(allData) <- c("subject", "activity", featReq.names)
+
+# turn activities & subjects into factors
+
+allData$activity <- factor(allData$activity, levels = actLabels[,1], labels = actLabels[,2])
+allData$subject <- as.factor(allData$subject)
+
+allData.melted <- melt(allData, id = c("subject", "activity"))
+allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
+
+#export the dataset
+
+write.table(allData.mean, "cleaneddataset.csv", row.names = TRUE, col.name = TRUE, sep = ",")
